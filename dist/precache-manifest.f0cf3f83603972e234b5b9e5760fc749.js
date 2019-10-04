d62455597b4d879018a27631507ef9d00992694e
ayoubkhan558 <ayoubkhan558@hotmail.com>
2019-10-04 21:30:05 +0500

    Dist Folder Added

---
+++ dist/precache-manifest.f0cf3f83603972e234b5b9e5760fc749.js
@@ -0,0 +1,366 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c809748baed901362de1e17b227cf33",
+    "url": "/assets/img/brand/brand4.png"
+  },
+  {
+    "revision": "61cd0b87afd1fca40824",
+    "url": "/js/about.9ab2e7ee.js"
+  },
+  {
+    "revision": "e92b8cd9da3e624fcded",
+    "url": "/js/app.f9f67ee5.js"
+  },
+  {
+    "revision": "9da4dd139b55da1c9225",
+    "url": "/js/chunk-vendors.6b677d59.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "178a97f4670f7d8d882739e3ee0e7c1d",
+    "url": "/assets/img/iconss/apple-icon-152x152.png"
+  },
+  {
+    "revision": "10a65f580e21f8781f359a690f83d247",
+    "url": "/assets/img/profile-img7.png"
+  },
+  {
+    "revision": "6c98bf4cdc7ff25a8502ec87200fc638",
+    "url": "/assets/js/jquery-1.12.4.min.js"
+  },
+  {
+    "revision": "a78ae00567147587be2e67fd5485f5b7",
+    "url": "/assets/img/work-bg.jpg"
+  },
+  {
+    "revision": "1e7b8687b6f21e13b31be8ab44026ab3",
+    "url": "/assets/js/isotope-3.0.4.min.js"
+  },
+  {
+    "revision": "5869c96cc8f19086aee625d670d741f9",
+    "url": "/assets/js/bootstrap-3.3.7.min.js"
+  },
+  {
+    "revision": "ba6cf724c8bb1cf5b084e79ff230626e",
+    "url": "/assets/js/magnific-popup.min.js"
+  },
+  {
+    "revision": "6a35dc92d10c9ff9a47bb49ffb6d8790",
+    "url": "/assets/js/modernizr.js"
+  },
+  {
+    "revision": "8d1f4a30a0bdba507110ce8c7bd3a599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff7f92d07b9bacad3080f2adaeb4f4d",
+    "url": "/assets/js/wow.min.js"
+  },
+  {
+    "revision": "fdb192a3600b57da051dc974617d1360",
+    "url": "/assets/js/waypoints.min.js"
+  },
+  {
+    "revision": "f42c75cfb0e8076577230dff9ee1bb4f",
+    "url": "/assets/js/easing.min.js"
+  },
+  {
+    "revision": "4f0e0b047f2ed8512d5119b31432bdcf",
+    "url": "/assets/js/counterup.min.js"
+  },
+  {
+    "revision": "c49ebcdde1f899143c091444fb9bbc97",
+    "url": "/assets/js/active.js"
+  },
+  {
+    "revision": "87dc40e4e7c147b71310ca409f653fc3",
+    "url": "/assets/img/testimonial/3.jpg"
+  },
+  {
+    "revision": "9bffb612e42b4a566fe7e8522bd58069",
+    "url": "/assets/img/testimonial/2.jpg"
+  },
+  {
+    "revision": "e3e14e616e1a1df73bc0ba2fe1849908",
+    "url": "/assets/img/testimonial/1.jpg"
+  },
+  {
+    "revision": "f0f28b3cd1e2b061a26e77d8bc53e5ab",
+    "url": "/assets/img/signature (8).png"
+  },
+  {
+    "revision": "a20217fd64452302ac317310a7566ba3",
+    "url": "/assets/img/signature (5).png"
+  },
+  {
+    "revision": "6abde5cdc0d78a6d8c80df60e0226add",
+    "url": "/assets/img/signature (19).png"
+  },
+  {
+    "revision": "fa5f517ddf84c325c9f4f8573f6530cc",
+    "url": "/assets/img/signature (14).png"
+  },
+  {
+    "revision": "d38338751fb8597ea77ce7f1da55a634",
+    "url": "/assets/img/signature (11).png"
+  },
+  {
+    "revision": "d38338751fb8597ea77ce7f1da55a634",
+    "url": "/assets/img/signature (12).png"
+  },
+  {
+    "revision": "548dae603a96c4c0dc1b4be6b76422cb",
+    "url": "/assets/img/signature (13).png"
+  },
+  {
+    "revision": "7ae125b5cc982b0f8b5ffb02b886f0cc",
+    "url": "/assets/img/signature (15).png"
+  },
+  {
+    "revision": "90a41d8c000f0a7caa15805523496643",
+    "url": "/assets/img/signature (17).png"
+  },
+  {
+    "revision": "ed884195daaf1803f4ff689b68f11a2d",
+    "url": "/assets/img/signature (18).png"
+  },
+  {
+    "revision": "3d2f06e1ef15a61563cb208647c88702",
+    "url": "/assets/img/signature (16).png"
+  },
+  {
+    "revision": "95afcc3436151b99c8886b2016c3b024",
+    "url": "/assets/img/signature (2).png"
+  },
+  {
+    "revision": "9e12d11c830855dfcc81adf48a3d8492",
+    "url": "/assets/img/signature (20).png"
+  },
+  {
+    "revision": "2dba404c6a52512b35ced307c1835ac2",
+    "url": "/assets/img/signature (3).png"
+  },
+  {
+    "revision": "a00c862cb65b559fa129f8a6d70f45ae",
+    "url": "/assets/img/signature (4).png"
+  },
+  {
+    "revision": "01ed4b094f89bd5792c60d81b2c38762",
+    "url": "/assets/img/signature (6).png"
+  },
+  {
+    "revision": "e48517fc6377a241473d46ddec737bb6",
+    "url": "/assets/img/signature (7).png"
+  },
+  {
+    "revision": "f657bb572f070f4d9873eb73122a93df",
+    "url": "/assets/img/signature (9).png"
+  },
+  {
+    "revision": "a00c862cb65b559fa129f8a6d70f45ae",
+    "url": "/assets/img/signature.png"
+  },
+  {
+    "revision": "2c594cfcc530858f616451aeefb3ddf3",
+    "url": "/assets/img/signature (10).png"
+  },
+  {
+    "revision": "dd64cf6ecbf0752e20cfb77c5f23e748",
+    "url": "/assets/img/signature (1).png"
+  },
+  {
+    "revision": "e92b8cd9da3e624fcded",
+    "url": "/css/app.722cd451.css"
+  },
+  {
+    "revision": "999565c98a9c3d6aefb4ecd4e916a622",
+    "url": "/assets/img/profile-img2.png"
+  },
+  {
+    "revision": "142582e56523340fea1d1b0a9ade9fb8",
+    "url": "/assets/img/brand/brand2.png"
+  },
+  {
+    "revision": "08ea6f9e0fe93c2c0a25e096528be3d7",
+    "url": "/assets/img/brand/brand6.png"
+  },
+  {
+    "revision": "eaf1893c6f6192edf0ff96b84d73b5c3",
+    "url": "/assets/img/iconss/android-icon-192x192.png"
+  },
+  {
+    "revision": "0c6ed80702658cb338ecb36f6e81e660",
+    "url": "/assets/img/brand/brand3.png"
+  },
+  {
+    "revision": "bceed15aff654bfefbba047c6786a7b3",
+    "url": "/assets/img/iconss/apple-icon-180x180.png"
+  },
+  {
+    "revision": "9e2ac783f64b7f97e414c7c6dd1f70dc",
+    "url": "/assets/img/iconss/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "9e2ac783f64b7f97e414c7c6dd1f70dc",
+    "url": "/assets/img/iconss/apple-icon.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/assets/img/iconss/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "ddee3b336d62bcd9e83be6687d93e337",
+    "url": "/assets/img/iconss/ms-icon-150x150.png"
+  },
+  {
+    "revision": "f98a9fbd3801df1709980c9f627b9ecb",
+    "url": "/assets/img/brand/brand5.png"
+  },
+  {
+    "revision": "3d0c170227008aac5f4f2cc5c5ec9dc8",
+    "url": "/assets/img/profile-img5.png"
+  },
+  {
+    "revision": "7e906d5289f14be4f5ef2aa40bea82d8",
+    "url": "/assets/img/iconss/ms-icon-70x70.png"
+  },
+  {
+    "revision": "d3c99bcbcd0ffa7affa0bc92eb92a08d",
+    "url": "/assets/img/iconss/ms-icon-144x144.png"
+  },
+  {
+    "revision": "2af14bc3fa7a3f0a69b5133b3e2ac16f",
+    "url": "/assets/img/iconss/favicon-32x32.png"
+  },
+  {
+    "revision": "5dbd4acbd9eb926f996d4878fd872ba3",
+    "url": "/assets/img/iconss/favicon-96x96.png"
+  },
+  {
+    "revision": "6aee38250422b0c76dccf083bcad25c3",
+    "url": "/assets/img/logo.png"
+  },
+  {
+    "revision": "c86448edb0ebeebe3d6b18641c5d6042",
+    "url": "/assets/img/portfolio/work-1.jpg"
+  },
+  {
+    "revision": "b6b3fa04bf3ee043daacba6191f8c7a1",
+    "url": "/assets/img/portfolio/work-2.jpg"
+  },
+  {
+    "revision": "79043eabab747481feefc5772b76071a",
+    "url": "/assets/img/portfolio/work-4.jpg"
+  },
+  {
+    "revision": "3e73bdbdeab7f79314106e44f9874b2f",
+    "url": "/assets/img/portfolio/work-5.jpg"
+  },
+  {
+    "revision": "f18e80a04570ea2b646c39ffdefe28e7",
+    "url": "/assets/img/portfolio/work-6.jpg"
+  },
+  {
+    "revision": "5a4211aaa8366da7143b475dd77afb6b",
+    "url": "/assets/img/logo2.png"
+  },
+  {
+    "revision": "bd1bdf3b6b62d30de0b69ee2130d1949",
+    "url": "/assets/img/profile-img6.png"
+  },
+  {
+    "revision": "b3e5a03f619e1e5e6557042eac0ef1b6",
+    "url": "/assets/img/iconss/ms-icon-310x310.png"
+  },
+  {
+    "revision": "e63c7e2ba7ed69ee4700ea54debc0c8b",
+    "url": "/assets/img/iconss/favicon-16x16.png"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/assets/img/iconss/browserconfig.xml"
+  },
+  {
+    "revision": "93a3fd52fa674916c4d348449b6fbe64",
+    "url": "/assets/img/iconss/apple-icon-72x72.png"
+  },
+  {
+    "revision": "69fbfa2241ca211c1737aa6b5c594031",
+    "url": "/assets/img/iconss/apple-icon-76x76.png"
+  },
+  {
+    "revision": "d6cf2ef47c456ff86c79243a5515aec8",
+    "url": "/assets/img/iconss/android-icon-48x48.png"
+  },
+  {
+    "revision": "5dbd4acbd9eb926f996d4878fd872ba3",
+    "url": "/assets/img/iconss/android-icon-96x96.png"
+  },
+  {
+    "revision": "349e89aead9b98600be396bb7fcc84fd",
+    "url": "/assets/img/iconss/apple-icon-60x60.png"
+  },
+  {
+    "revision": "83ffa3155c77514c523b8bba246dc476",
+    "url": "/assets/img/iconss/apple-icon-57x57.png"
+  },
+  {
+    "revision": "1ee5368ae691424fc49e6bc764676000",
+    "url": "/assets/img/iconss/apple-icon-114x114.png"
+  },
+  {
+    "revision": "de64afaaf61091c8461e5f55dd68b686",
+    "url": "/assets/img/portfolio/work-3.jpg"
+  },
+  {
+    "revision": "eadfc646631cefa262282d0afe5dadbc",
+    "url": "/assets/img/profile-img.png"
+  },
+  {
+    "revision": "f3d0c5c2a984eefc2d3e120313d3881d",
+    "url": "/assets/img/iconss/apple-icon-120x120.png"
+  },
+  {
+    "revision": "487739480ef972699448d0f4302c33f5",
+    "url": "/assets/img/profile-img3.png"
+  },
+  {
+    "revision": "d3c99bcbcd0ffa7affa0bc92eb92a08d",
+    "url": "/assets/img/iconss/apple-icon-144x144.png"
+  },
+  {
+    "revision": "fc21525c19e7d39bb1a7944725ea63d3",
+    "url": "/assets/img/iconss/android-icon-36x36.png"
+  },
+  {
+    "revision": "93a3fd52fa674916c4d348449b6fbe64",
+    "url": "/assets/img/iconss/android-icon-72x72.png"
+  },
+  {
+    "revision": "d3c99bcbcd0ffa7affa0bc92eb92a08d",
+    "url": "/assets/img/iconss/android-icon-144x144.png"
+  },
+  {
+    "revision": "236ef6467f1b714dc18bb0a47373efcb",
+    "url": "/assets/img/brand/brand1.png"
+  },
+  {
+    "revision": "14ed7c3e04584566717d4544f08d40ad",
+    "url": "/assets/img/blog/blog-3.jpg"
+  },
+  {
+    "revision": "42eceb3dac506cdf5d5e5e22dc72e4a4",
+    "url": "/assets/img/blog/blog-2.jpg"
+  },
+  {
+    "revision": "b8d973dec81fecb90a8b4ab6b5139492",
+    "url": "/assets/img/blog/blog-1.jpg"
+  },
+  {
+    "revision": "a425b4d43cf08e57f759afe8b6b0a40a",
+    "url": "/assets/img/Untitled-1.psd"
+  }
+];
